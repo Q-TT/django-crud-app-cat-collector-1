303c930f9763ad74dcae1564947b18f3a58fe5a0
Astrid Ge <qiutongg@umich.edu>
2025-02-10 23:22:07 +0800

    create database

--- catcollector_1/settings.py
+++ catcollector_1/settings.py
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'catcollector_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catcollector',
     }
 }
+
 
 
 # Password validation
